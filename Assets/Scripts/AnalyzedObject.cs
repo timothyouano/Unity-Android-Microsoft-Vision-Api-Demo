b2b78656e491f0e0ae0020edc47683e09ccc3558
DESKTOP-RUCEG1P\Timothy <timothyouano@gmail.com>
2018-01-14 09:48:04 +0800

    January 14, 2018 Commit - Master branch free to modify

---
+++ Assets/Scripts/AnalyzedObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnalyzedObject : MonoBehaviour {
+
+    public List<ObjectCategory> categories { get; private set; }
+    public Text txt = GameObject.Find("Result").GetComponent<Text>();
+
+    public AnalyzedObject(JSONObject cat)
+    {
+        categories = ConvertScoresToCategoryList(cat);
+        GetHighestObjectCategory();
+    }
+
+    public List<ObjectCategory> ConvertScoresToCategoryList(JSONObject cat)
+    {
+        List<ObjectCategory> visionGuesses = new List<ObjectCategory>();
+        for (int i = 0; i < cat.Count; i++)
+        {
+            JSONObject lineObj = cat[i];
+            ObjectCategory c = new ObjectCategory(lineObj.list[0].ToString(), float.Parse(lineObj.list[1].ToString()));
+            visionGuesses.Add(c);
+            Debug.Log(c.ToString());
+        }
+        return visionGuesses;
+    }
+
+    public ObjectCategory GetHighestObjectCategory()
+    {
+        ObjectCategory max = categories[0];
+        foreach (ObjectCategory e in categories)
+        {
+            if (e.score > max.score)
+            {
+                max = e;
+            }
+        }
+        Debug.Log("Best ObjectCategory Guess: " + max.ToString());
+        txt.text = max.ToString();
+        return max;
+    }
+}
---
+++ Assets/Scripts/VisionAPI.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionAPI : MonoBehaviour {
+
+    public string ApiKey = "39dd295b44d842a09a3ec5368aa3564d"; // replace with your own key
+    public string emotionURL = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/tag";
+
+    public List<AnalyzedObject> imageCategories = new List<AnalyzedObject>();
+
+    public string fileName { get; private set; }
+    string responseData;
+
+    // Use this for initialization
+    void Start()
+    {
+        fileName = System.IO.Path.Combine(Application.streamingAssetsPath, "lion.jpg");
+    }
+
+    public void getData()
+    {
+        StartCoroutine(GetDataFromImages());
+    }
+
+    IEnumerator GetDataFromImages()
+    {
+        WWW reader = new WWW(fileName);
+        while (!reader.isDone) { }
+        byte[] bytes = reader.bytes;
+
+        var headers = new Dictionary<string, string>() {
+            { "Ocp-Apim-Subscription-Key", ApiKey },
+            { "Content-Type", "application/octet-stream" }
+        };
+
+        WWW www = new WWW(emotionURL, bytes, headers);
+
+        yield return www;
+        responseData = www.text;
+        ParseJSONData(responseData);
+    }
+
+    // Parsing Data
+    public void ParseJSONData(string respString)
+    {
+        JSONObject dataArray = new JSONObject(respString);
+        AnalyzedObject _imageObject = ConvertObjectToFoundImageObject(dataArray);
+    }
+
+    private AnalyzedObject ConvertObjectToFoundImageObject(JSONObject obj)
+    {
+        JSONObject _categories = obj.list[0]; // Get the list of categories
+        return new AnalyzedObject(_categories);
+    }
+
+}
+
+public class ObjectCategory
+{
+    public string name { get; private set; }
+    public float score { get; private set; }
+
+    public ObjectCategory(string name, float score)
+    {
+        this.name = name;
+        this.score = score;
+    }
+
+    override public string ToString()
+    {
+        return name + " : " + score;
+    }
+}
